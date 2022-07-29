49c71a375120bdbfcc73011d4f89f757904f27f4
Anna <anna.vasi4ko@icloud.com>
2022-07-29 13:25:52 +0300

    Initial Commit

---
+++ ChartsDemo/ChartsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsDemoApp.swift
+//  ChartsDemo
+//
+//  Created by Vasichko Anna on 29.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct ChartsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
